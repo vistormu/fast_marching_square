adaea65910a73b24902eba9efbcba77986021b27
Vistor <vistormu@gmail.com>
2023-03-27 12:57:47 +0200

    0.1.1

--- changelog.md
+++ changelog.md
@@ -1,2 +1,5 @@
+# 0.1.1
+- Fixed a bug for the path to the binaries
+
 # 0.1.0
 - First version of the package!
--- fm2/__init__.py
+++ fm2/__init__.py
@@ -3,11 +3,13 @@ from .fm2 import FM2
 
 def _create_binary_txt() -> None:
     import os
+    import fm2
 
-    cwd: str = os.getcwd()
+    path: str = os.path.abspath(fm2.__file__)
+    path = path.split('__init__.py')[0]
 
     with open('FileHFM_binary_dir.txt', 'w+') as file:
-        file.write(cwd+'/fm2/bin/')
+        file.write(path+'bin/')
 
 
 _create_binary_txt()
